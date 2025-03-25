13edd1dc7356efe2d885da24f94f33d734d868f3
Phrechu <aem.bouhamid@gmail.com>
2025-03-25 18:35:41 +0000

    feat: Implement automatic PiP script and cleanup boilerplate

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    commands: {
+      _execute_action: {
+        suggested_key: {
+          windows: "Alt+P",
+          mac: "Alt+P",
+          chromeos: "Alt+P",
+          linux: "Alt+P",
+        },
+      },
+    },
+    permissions: ["activeTab", "scripting"],
+    host_permissions: ["<all_urls>"],
+    manifest_version: 3,
+  },
   srcDir: "src",
   extensionApi: "chrome",
 });
