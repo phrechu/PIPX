0a170a7b0fb9adfa8bfbd6f1c558a541219be3d7
Phrechu <aem.bouhamid@gmail.com>
2025-07-05 00:23:02 +0100

    update name and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from "wxt";
+import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: "PIPX - better PiP experience",
-    permissions: ["storage", "contextMenus"],
+    name: 'PIPX - Take Control of Picture-in-Picture',
+    permissions: ['storage', 'contextMenus'],
     action: {
       default_icon: {
-        "16": "icon/16.png",
-        "32": "icon/32.png",
-        "48": "icon/48.png",
-        "96": "icon/96.png",
-        "128": "icon/128.png",
+        '16': 'icon/16.png',
+        '32': 'icon/32.png',
+        '48': 'icon/48.png',
+        '96': 'icon/96.png',
+        '128': 'icon/128.png',
       },
     },
     commands: {
       _execute_action: {
         suggested_key: {
-          windows: "Alt+P",
-          mac: "Alt+P",
-          chromeos: "Alt+P",
-          linux: "Alt+P",
+          windows: 'Alt+P',
+          mac: 'Alt+P',
+          chromeos: 'Alt+P',
+          linux: 'Alt+P',
         },
       },
     },
   },
   manifestVersion: 3,
-  srcDir: "src",
-});
+  srcDir: 'src',
+})
