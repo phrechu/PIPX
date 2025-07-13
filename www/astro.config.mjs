1c621268df98ffcdc2abdb3d2ebbb20cd7a3a2e9
Phrechu <aem.bouhamid@gmail.com>
2025-07-13 07:26:46 +0100

    fix: update dotenv config

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ quiet: true });
 
 import sitemap from '@astrojs/sitemap';
 
